f2fc57e2f01e841f0f15dbc32453a3fb532a5e62
cbrow353 <cbrow353@uncc.edu>
2024-02-27 08:16:51 -0500

    define version for mac

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -32,6 +32,12 @@ Window::Window(int SCR_WIDTH, int SCR_HEIGHT, const char* title){
         std::cerr << "Failed to initialize GLFW" << std::endl;
         exit(-1);
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, title, NULL, NULL);
     if (window == NULL)
     {
