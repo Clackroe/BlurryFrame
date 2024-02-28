05e0116d7aaa8e930285a7d747fc8862d7f5f0ad
cbrow353 <cbrow353@uncc.edu>
2024-02-27 20:04:29 -0500

    remove init hints

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -94,16 +94,6 @@ void Window::setHeight(int h){SCR_HEIGHT= h;}
 
 
 void Window::initOpenGL(){
-    
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
