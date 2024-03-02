c51c09631a950e6a3b2557eac57b006e3975e8d8
Clackroe <clackroe@gmail.com>
2024-03-02 18:23:23 -0500

    Udated Preamke.lua

--- premake5.lua
+++ premake5.lua
@@ -1,58 +1,57 @@
-require "export-compile-commands"
 workspace "BlurryFrame"
-    configurations { "Debug", "Release" }
-    startproject "Blurry"
+configurations { "Debug", "Release" }
+startproject "Blurry"
 
-    flags { "MultiProcessorCompile" }
+flags { "MultiProcessorCompile" }
 
-    filter "configurations:Debug"
-        defines { "DEBUG", "DEBUG_SHADER" }
-        symbols "On"
+filter "configurations:Debug"
+defines { "DEBUG", "DEBUG_SHADER" }
+symbols "On"
 
-    filter "configurations:Release"
-        defines { "RELEASE" }
-        optimize "Speed"
-        flags { "LinkTimeOptimization" }
+filter "configurations:Release"
+defines { "RELEASE" }
+optimize "Speed"
+flags { "LinkTimeOptimization" }
 
 project "Blurry"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++17"
-	architecture "x86_64"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++17"
+architecture "x86_64"
 
-    targetdir "bin/%{cfg.buildcfg}"
-    objdir "obj/%{cfg.buildcfg}"
+targetdir "bin/%{cfg.buildcfg}"
+objdir "obj/%{cfg.buildcfg}"
 
-    includedirs {"include/", "libs/glad/include/", "libs/glfw/include/", "libs/glm/", "libs/imgui/"}
-    
-    files {
-        "src/**.cpp",
-        "src/image/**.cpp",
-        "src/graphics/**.cpp",
-        "include/**.hpp",
-        "include/**.h"
-    }
+includedirs { "include/", "libs/glad/include/", "libs/glfw/include/", "libs/glm/", "libs/imgui/" }
 
-    if os.host() == "linux" then
-        links { "IMGUI", "GLFW", "GLAD", "GLM", "dl", "pthread" }
-        defines { "_X11" }
-        include "libs/glfw.lua"
-        include "libs/glad.lua"
-        include "libs/glm.lua"
-    end
-    
-    if os.host() == "windows" then
-        links { "GLFW", "GLAD", "GLM", "IMGUI" }
-        defines { "_WINDOWS" }
-        include "libs/glfw.lua"
-    end
-    
-    if os.host() == "macosx" then
-        links {"libs/glfw.3.3", "IMGUI", "GLAD", "GLM"}
-        defines { "_GLFW_COCOA" }
-    end
-    
-    filter {}
+files {
+    "src/**.cpp",
+    "src/image/**.cpp",
+    "src/graphics/**.cpp",
+    "include/**.hpp",
+    "include/**.h"
+}
+
+if os.host() == "linux" then
+    links { "IMGUI", "GLFW", "GLAD", "GLM", "dl", "pthread" }
+    defines { "_X11" }
+    include "libs/glfw.lua"
+    include "libs/glad.lua"
+    include "libs/glm.lua"
+end
+
+if os.host() == "windows" then
+    links { "GLFW", "GLAD", "GLM", "IMGUI" }
+    defines { "_WINDOWS" }
+    include "libs/glfw.lua"
+end
+
+if os.host() == "macosx" then
+    links { "libs/glfw.3.3", "IMGUI", "GLAD", "GLM" }
+    defines { "_GLFW_COCOA" }
+end
+
+filter {}
 
 include "libs/glad.lua"
 include "libs/glm.lua"
@@ -99,7 +98,7 @@ newaction {
 newaction {
     trigger = "run",
     description = "Run in Debug Mode",
-    execute = function ()
+    execute = function()
         print("Running DEBUG...")
         os.execute("./bin/Debug/Blurry")
     end
@@ -108,7 +107,7 @@ newaction {
 newaction {
     trigger = "build",
     description = "Build in debug mode",
-    execute = function ()
+    execute = function()
         print("Building in DEBUG")
         os.execute("make")
     end
